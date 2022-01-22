08b4d669376b9c339ea0062d89ac95c837646c63
Team-Lad <89805316+Team-Lad@users.noreply.github.com>
2022-01-22 12:25:20 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,13 +3,22 @@ class script(object):
 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+    ABOUT_TXT = """ɴᴀᴍᴇ : <a href='https://t.me/addstickers/sp750c68b5c46efdbac504a91b0cbd2613_by_stckrRobot'>ᴛᴢᴜʏᴜ</a>
+
+    
+➥ ᴄʀᴇᴅɪᴛs : <a href='tg://settings'>ʏᴏᴜ</a>
+
+➥ ᴇᴅɪᴛɪɴɢ : <a href='http://t.me/cat_of_tg'>ᴄᴀᴛ ʙᴏɪ</a>
+
+➥ ᴏᴡɴᴇʀ  : <a href='http://t.me/cat_bio'>ᴘᴏɪᴤᴏɴ</a>
+
+➥ ʟᴀɴɢᴜᴀɢᴇ : <a href='docs.python.org'>ᴘʏᴛʜᴏɴ</a>
+
+➥ ʟɪʙʀᴀʀʏ :  <a href='docs.pyrogram.org'>ᴘʏʀᴏɢʀᴀᴍ</a>
+
+➥ ᴤᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='http://t.me/kids_heaven_chat'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>
+
+"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Eva Maria is a open source project. 
 - Source - https://github.com/EvamariaTG/EvaMaria  
